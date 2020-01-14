e3d87eefe7b4be6adfe2a5ff3c9ef7ce0ac4402d
121314RibenSB <56599304+121314RibenSB@users.noreply.github.com>
2020-01-14 11:16:10 +0100

    Add files via upload

---
+++ Tarefa-2/escripito.js
@@ -0,0 +1,13 @@
+/* 
+	Primeira tarefa
+	Nome do ficheiro: escripito.js
+	Usado: alert()
+*/
+alert("Moi boas a todos");
+/*funciona copia e pega o que vai entre comillas*/
+alert("que tal?");
+/*funciona copia e pega o que vai entre comillas*/
+alert("2+2");
+/*como está arriba con comillas copia lo que halla entre la comillas*/
+alert(2+2);
+/*como está sin comillas hace la operacion osea 4*/
