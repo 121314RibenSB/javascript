a847d722d683dacef1b1636497fa833fe451c402
121314RibenSB <56599304+121314RibenSB@users.noreply.github.com>
2020-01-14 11:15:48 +0100

    Add files via upload

---
+++ Tarefa-1/escripito.js
@@ -0,0 +1,16 @@
+/* 
+	Primeira tarefa
+	Nome do ficheiro: escripito.js
+	Usado: alert()
+*/
+var gastoHoxe;
+gastoHoxe = 10;
+alert(gastoHoxe);
+/*Declara la variable y se ejecuta perfectamente. Aparece un 10 como se ordena*/
+
+var gastoHoxe = 22.22;
+var gastoOnte = 33.33;
+var total = gastoHoxe + gastoOnte;
+alert(total);
+
+/* Ejecuta la variable y suma los 2 factores dando el resultado en la alerta*/
