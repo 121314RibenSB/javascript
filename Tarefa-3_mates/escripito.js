edec4e5b0878be3dbcbada4d226ddde37c4cd58b
121314RibenSB <56599304+121314RibenSB@users.noreply.github.com>
2020-01-14 11:46:20 +0100

    Add files via upload

---
+++ Tarefa-3_mates/escripito.js
@@ -0,0 +1,34 @@
+/* 
+	Código en JavaScript
+	Nome do ficheiro: escripito.js
+	Usado: 
+*/
+var moito = 9 ;
+var raiz = Math.sqrt(moito);
+alert(raiz);
+/*Ejemplo*/
+
+var aaa = 2.4567;
+var sss = 3.5672;
+var jjj = 7.2563;
+var pi = Math.PI;
+
+var prueba1 = Math.sqrt(aaa);
+alert(prueba1);
+/*Raíz Cuadrada*/
+
+var prueba2 = Math.pow(sss,2);
+alert(prueba2);
+/*Cuadrado*/
+
+var prueba3 = Math.round(pi);
+alert(prueba3);
+/*Número PI*/
+
+var prueba4 = Math.min(aaa, sss, jjj);
+alert(prueba4);
+/*Función que responde con la menor de las variables*/
+
+var prueba5 = Math.random(0 ,3);
+alert(prueba5);
+/*número aleatorio entre 0 y 3*/
