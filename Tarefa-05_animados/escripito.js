979ab8927a45da376ddad5fa79d38f50f72bc112
121314RibenSB <56599304+121314RibenSB@users.noreply.github.com>
2020-01-16 09:15:10 +0100

    Add files via upload

---
+++ Tarefa-05_animados/escripito.js
@@ -0,0 +1,21 @@
+/* 
+	Código en JavaScript
+	Nome do ficheiro: escripito.js
+	Usado: 
+*/
+
+var nome;
+nome = prompt("Como te chamas?   ");
+alert("Guau... que nome máis interesante " + nome);
+alert("Ti es unha grande persoa " + nome);
+alert("Vales moito " + nome);
+alert("Non hai ninguén máis elegante ca " + nome);
+alert("e... " + nome + ", o mundo adórate!");
+
+/* Al hacer esto te pregunta como te llamas y tu escribirás lo que 
+quieras por ejemplo Sergio y luego le das a aceptar entonces te pondrá
+Guau... que nome máis interesante Sergio
+Ti es unha grande persoa Sergio
+Vales moito Sergio
+Non hai ninguén máis elegante ca Sergio
+e... Sergio o mundo adórate! */
