59530d644b452507d51ea69a2ed5cc7ff61fdd34
121314RibenSB <56599304+121314RibenSB@users.noreply.github.com>
2020-01-17 10:05:59 +0100

    Add files via upload

---
+++ Tarefa-06/escripito.js
@@ -0,0 +1,29 @@
+ /*
+	Código en JavaScript
+	Nome do ficheiro: escripito.js
+	Usado: 
+*/
+var posicion = 3;
+if(posicion == 1) {
+	alert("Medalla de ouro");
+} else {
+	alert("Non es o campeón ");
+}
+Declaramos la variable posición y le decimos que es 3 luego decimos si coincide (if) 
+con 1 pondrá medalla de ouro y en caso contrario (else) no es campeón
+
+
+var desexo = confirm("Queres seguer nesta páxina?");
+if (desexo == true){
+	alert("Aquí nos quedamos");
+} else {
+	alert("De verdade acabas de premer en Cancel ?");
+}
+
+var deseo = confirm("chove?");
+if(deseo == true){
+	alert("Que fatidio");
+} else {
+	alert("Registrate");
+}
+
