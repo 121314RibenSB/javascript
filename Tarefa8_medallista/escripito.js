502b1aa342248e4a9c9857bf176dc48f3e2ffdad
121314RibenSB <56599304+121314RibenSB@users.noreply.github.com>
2020-01-20 10:41:58 +0100

    Add files via upload

---
+++ Tarefa8_medallista/escripito.js
@@ -0,0 +1,21 @@
+/* 
+	Código en JavaScript
+	Nome do ficheiro: escripito.js
+	Usado: 
+*/
+function TipoMedalla(posicion){
+	if(posicion == 1)
+	{medalla = "Medalla de ouro";}
+	else if (posicion == 2)
+	{medalla = "Medalla de prata";}
+	else if (posicion == 3)
+	{medalla = "Medalla de bronce";}
+	else { medalla = "Mala sorte, outra vez será";}
+}
+var posicion = prompt("En que posto quedaches?");
+posicion = parseFloat(posicion);
+
+var funcionaza = TipoMedalla(posicion);
+document.write(posicion + "º");
+document.write(medalla);
+
